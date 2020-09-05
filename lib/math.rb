528efb543f07e7591360bd3a48c3c31de29f5e62
Jordan Dains <dains.jordan2@gmail.com>
2020-09-05 19:40:12 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,29 +1,29 @@
 def addition(num1, num2)
   addition = num1 + num2 
-  puts addition
+  return addition
 end
 
 def subtraction(num1, num2)
   subtraction = num2 - num1
-  puts subtraction
+  return subtraction
 end
 
 def multiplication(num1, num2)
   multiplication = num1 * num2
-  puts multiplication
+  return multiplication
 end
 
 def division(num1, num2)
   division = num2 / num1
-  puts division
+  return division
 end
 
 def modulo(num1, num2)
   modulo = num2 % num1
-  puts modulo
+  return modulo
 end
 
 def square_root(num)
   square_root = Math.sqrt(num)
-  puts square_root
+  return square_root
 end
